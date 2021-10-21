a1d6023cf526190a972941502afd47bb9fe6d2f8
yusuf963 <mohammad963yusuf@gmail.com>
2021-10-21 22:42:02 +0100

    add bd folder and install pg lib

--- server/views/resturants.js
+++ server/views/resturants.js
@@ -1,24 +1,25 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('response all data')
-})
+  res.send('response all data');
+});
 
 router.post('/', (req, res) => {
-  res.send('post data')
-})
+  res.send('hi, you created  new record');
+  console.log(req.body.number);
+});
 
 router.get('/:id', (req, res) => {
-  res.send('get single record')
-})
+  res.send('get single record');
+});
 
 router.patch('/:id', (req, res) => {
-  res.send('update single record')
-})
+  res.send('update single record');
+});
 
 router.delete('/:id', (req, res) => {
-  res.send('delete single record')
-})
+  res.send('delete single record');
+});
 
 module.exports = router;
