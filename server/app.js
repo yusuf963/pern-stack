ce75fae65a77bb143508a79d9b5faf2d5860770c
yusuf963 <mohammad963yusuf@gmail.com>
2021-10-21 22:40:08 +0100

    import route, setup middleware; express.json

--- server/app.js
+++ server/app.js
@@ -1,16 +1,15 @@
-const { json } = require('express')
-require('dotenv').config()
-const express = require('express')
-const { ConnectionStates } = require('mongoose')
-const router = require('./views/resturants')
+const { json } = require('express');
+require('dotenv').config();
+const express = require('express');
+const router = require('./views/resturants');
 
-const app = express()
+const app = express();
 //Middleware
-app.use(express.json())
+app.use(express.json());
 
 // import routers
-app.use('/api/v1/resrurant', router)
-const port = process.env.PORT || 5000
+app.use('/api/v1/resrurants', router);
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`App is running on port ${port}`)
-})
+  console.log(`App is running on port ${port}`);
+});
