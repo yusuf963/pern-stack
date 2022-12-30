fc06e88f5010dab3bf5bc16014f73723a6d30f7a
yusuf963 <mohammad963yusuf@gmail.com>
2022-12-30 00:52:20 +0000

    create server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,21 @@
-const { json } = require('express');
-require('dotenv').config();
-const express = require('express');
-const router = require('./views/resturants');
+import express from 'express';
+import router from './views/routes.js';
+import { sqlClient } from './db/index.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// sqlClient.query("INSERT INTO todo (description, completed, creaeted_at) values( 'Play Tennis', false, Now())", function (err, result) {
+//   if (err) {
+//     return console.error('error running query', err);
+//   }
+//   console.log(result.rows[0]);
+// })
 
 const app = express();
-//Middleware
-app.use(express.json());
 
-// import routers
-app.use('/api/v1/resrurants', router);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`App is running on port ${port}`);
