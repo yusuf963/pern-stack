1346612c72a2b575c3ef727a109c16c7256c0b1d
yusuf963 <mohammad963yusuf@gmail.com>
2023-03-13 23:57:13 +0000

    add commit

--- client/src/components/TodoItem.tsx
+++ client/src/components/TodoItem.tsx
@@ -3,21 +3,54 @@ import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
 import EditIcon from '@mui/icons-material/Edit';
 import { Drawer } from '@mui/material';
+import { useMutation, useQuery } from 'react-query';
+import { Itodo,Ifetch } from '../type';
 
-
-import { Itodo } from '../type';
 
 const Todo = (item: Itodo) => {
     const { id, description, created_at, completed } = item;
-
+    
     const handleBoolean = (value: boolean) => {
         if (value) return 'completed';
         return 'not completed';
     };
 
+    const getOneTodo = async ():Promise<Ifetch> => {
+        return await (await fetch(`http://localhost:5000/${1}`)).json();
+    };  
+    const { data, status, isLoading, error } = useQuery<Ifetch, Error>(['todos', 1], getOneTodo);
+
+
+
+    const deleteTodo = async(id: string) => {
+        await fetch(`http://localhost:5000/${id}`, {
+            method: 'DELETE',
+        });
+        console.log('delete');
+    };
+
+    const data1 = {description: 'test', completed: false};
+    const PostTodo = async ()=>{
+        await fetch('http://localhost:5000/',{headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(data1),
+            method: 'POST',
+        });
+        console.log('post');
+    };
+
+
+
+    // const { data, status, isLoading, error } = useQuery<Itodo, Error>(['todos', id], getOneTodo);
+
+
+    // const mutation =  useMutation('delete-todos', deleteTodo);
+
     return (
         <div key={id}>
-            <Drawer>
+            <Button onClick={()=> PostTodo()} size="small" color="warning" variant="text" aria-label="outlined primary button group">
+                <AddIcon />
+            </Button>
+            <div>
                 <p>{id}</p>
                 <p>{description}</p>
                 <p>{created_at}</p>
@@ -28,10 +61,13 @@ const Todo = (item: Itodo) => {
                 <Fab color="primary" aria-label="add">
                     -
                 </Fab>
-                <Fab onClick={()=> console.log('hi')} color="secondary" aria-label="edit">
+                <Fab onClick={()=> deleteTodo(id)} color="secondary" aria-label="edit">
                     <EditIcon />
                 </Fab>
-            </Drawer>
+                <Fab onClick={()=> getOneTodo(id)} color="secondary" aria-label="edit">
+                    Details
+                </Fab>
+            </div>
         </div>
     );
 };
--- server/controllers/todo.js
+++ server/controllers/todo.js
@@ -1,120 +1,124 @@
-import { sqlClient } from '../db/index.js';
+import { sqlClient } from "../db/index.js";
 
-const table = '"public"."todo"'
-
+const table = '"public"."todo"';
 
 const getTime = () => {
-    sqlClient.query('SELECT NOW() AS "theTime"', function (err, result) {
-        if (err) {
-            return console.error('error running query', err);
-        }
-        console.log(result.rows[0].theTime);
-    });
-
-
-
-}
+  sqlClient.query('SELECT NOW() AS "theTime"', function (err, result) {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    console.log(result.rows[0].theTime);
+  });
+};
 
 const getAllTodos = (_req, res, next) => {
+  sqlClient.query('SELECT * FROM "public"."todo"', function (err, result) {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    if (result.rowCount === 0) return res.json({ message: "Todo not found" });
 
-    sqlClient.query('SELECT * FROM "public"."todo"', function (err, result) {
-        if (err) {
-            return console.error('error running query', err);
-        }
-        if (result.rowCount === 0) return res.json({ message: 'Todo not found' })
-
-        return res.json({ result: result.rows, message: 'Todos fetched successfully', success: true });
-        // client.end();
+    return res.json({
+      result: result.rows,
+      message: "Todos fetched successfully",
+      success: true,
     });
-
-
-
-
-}
+    // client.end();
+  });
+};
 
 const getOneTodo = (req, res, next) => {
-    const id = req.params.id;
-    if (!id.match(/^[0-9]+$/)) {
-        return res.json({ message: 'Todo not found' })
+  const id = Number(req.params.id);
+  //   if (!id.match(/^[0-9]+$/)) {
+  //     return res.json({ message: "Todo not found" });
+  //   }
+
+  sqlClient.query(
+    'SELECT * FROM "public"."todo" WHERE id= $1 ',
+    [id],
+    function (err, result) {
+      if (err) {
+        return console.error("error running query", err);
+      }
+      if (result.rowCount === 0) return res.json({ message: "Todo not found" });
+      console.log(result.rows);
+      return res.json(result.rows);
+      // client.end();
     }
-
-    sqlClient.query('SELECT * FROM "public"."todo" WHERE id= $1 ', [id], function (err, result) {
-        if (err) { return console.error('error running query', err) }
-        if (result.rowCount === 0) return res.json({ message: 'Todo not found' })
-
-        return res.json(result.rows);
-        // client.end();
-    });
-
-
-
-
-}
+  );
+};
 
 const createTodo = (req, res, next) => {
-    const { description, completed } = req.body;
-    console.log(req.body)
+  const { description, completed } = req.body;
+  console.log(req.body);
 
-    sqlClient.query('INSERT INTO "public"."todo" (description, completed, 	created_at) values($1, $2, now())',
-        [description, completed], function (err, result) {
-            if (err) {
-                return console.error('error running query', err);
-            }
+  sqlClient.query(
+    'INSERT INTO "public"."todo" (description, completed, 	created_at) values($1, $2, now())',
+    [description, completed],
+    function (err, result) {
+      if (err) {
+        return console.error("error running query", err);
+      }
 
-            return res.json({ message: 'Todo created successfully', success: true });
-            // client.end();
-        });
-
-
-
-}
+      return res.json({ message: "Todo created successfully", success: true });
+      // client.end();
+    }
+  );
+};
 
 const updateTodo = (req, res, next) => {
-    const id = req.params.id;
-    if (!id.match(/^[0-9]+$/)) {
-        return res.json({ message: 'Todo not found' })
+  const id = req.params.id;
+  if (!id.match(/^[0-9]+$/)) {
+    return res.json({ message: "Todo not found" });
+  }
+
+  const { description, completed } = req.body;
+
+  sqlClient.query(
+    'UPDATE "public"."todo" SET description = $1, completed = $2 WHERE id= $3',
+    [description, completed, id],
+    function (err, result) {
+      if (err) {
+        return console.error("error running query", err);
+      }
+      sqlClient.query(
+        'SELECT * FROM "public"."todo" WHERE id = $1',
+        [id],
+        function (err, result) {
+          if (err) {
+            return console.error("error running query", err);
+          }
+          if (result.rowCount === 0)
+            return res.json({ message: "Todo not found" });
+          return res.json({
+            result: result.rows,
+            message: "Todo updated successfully",
+            success: true,
+          });
+        }
+      );
     }
-
-    const { description, completed } = req.body;
-
-    sqlClient.query('UPDATE "public"."todo" SET description = $1, completed = $2 WHERE id= $3',
-        [description, completed, id], function (err, result) {
-            if (err) {
-                return console.error('error running query', err);
-            }
-            sqlClient.query('SELECT * FROM "public"."todo" WHERE id = $1', [id], function (err, result) {
-                if (err) {
-                    return console.error('error running query', err);
-                }
-                if (result.rowCount === 0) return res.json({ message: 'Todo not found' })
-                return res.json({ result: result.rows, message: 'Todo updated successfully', success: true });
-
-            })
-        });
-
-
-
-}
+  );
+};
 
 const deleteTodo = (req, res, next) => {
-    const id = req.params.id;
-    if (!id.match(/^[0-9]+$/)) {
-        return res.json({ message: 'Todo not found' })
+  const id = req.params.id;
+  if (!id.match(/^[0-9]+$/)) {
+    return res.json({ message: "Todo not found" });
+  }
+
+  sqlClient.query(
+    'DELETE FROM "public"."todo" WHERE id= $1',
+    [id],
+    function (err, result) {
+      if (err) {
+        return console.error("error running query", err);
+      }
+      if (result.rowCount === 0) return res.json({ message: "Todo not found" });
+
+      return res.json({ message: "Todo deleted successfully" });
     }
+  );
+};
 
-    sqlClient.query('DELETE FROM "public"."todo" WHERE id= $1', [id], function (err, result) {
-        if (err) {
-            return console.error('error running query', err);
-        }
-        if (result.rowCount === 0) return res.json({ message: 'Todo not found' })
-
-        return res.json({ message: 'Todo deleted successfully' });
-    });
-
-
-
-
-}
-
-
-export { getTime, getAllTodos, getOneTodo, createTodo, updateTodo, deleteTodo }
+export { getTime, getAllTodos, getOneTodo, createTodo, updateTodo, deleteTodo };
